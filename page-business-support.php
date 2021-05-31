e47e0e4871a86c542f01ac5f4c8adfa8827d21c4
Grant Corley <grantcorley@gmail.com>
2021-05-31 19:16:22 -0400

    created Business Support page with subpage callouts

--- page-business-support.php
+++ page-business-support.php
@@ -15,7 +15,7 @@ get_header();
 
 ?>
 
-<div class="page-business-support">
+<div class="page-business-support custom-content">
 
 <article class="page hardcoded type-page status-publish hentry entry">
 
@@ -30,39 +30,40 @@ get_header();
 
 	<div class="callouts">
 
-<div class="callout shop">
-<div class="bgimage"></div>
-<div class="content">
-  <div class="lockup">
-	<h2>Available Buildings</h2>
-  </div><!--end lockup-->
+	<a class="callout available-buildings" href="/business-support/available-buildings/">
+	<div class="bgimage"></div>
+	<div class="content">
+	<div class="lockup">
+		<h2>Available Buildings</h2>
+	</div><!--end lockup-->
 
-  <div class="cta"> <!-- ==================== -->
-	
-  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-		viewBox="0 0 356 355" style="enable-background:new 0 0 356 355;" xml:space="preserve">
-	  <style type="text/css">
-		.st0{fill:#F38F1D;}
-		.st1{fill:#FFFFFF;}
-	  </style>
-	  <g id="Layer_1" focusable="false">
-		<g>
-		  <polygon class="st0" points="355.9,0 0.9,355 355.9,355 		"/>
+	<div class="cta"> <!-- ==================== -->
+	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+			viewBox="0 0 356 355" style="enable-background:new 0 0 356 355;" xml:space="preserve">
+		<style type="text/css">
+			.st0{fill:#F38F1D;}
+			.st1{fill:#FFFFFF;}
+		</style>
+		<g id="Layer_1" focusable="false">
+			<g>
+			<polygon class="st0" points="355.9,0 0.9,355 355.9,355 		"/>
+			</g>
 		</g>
-	  </g>
-	  <g id="Layer_2">
-		<path class="st1" d="M309.7,185.7h13.9c5.9,0,10.6,4.7,10.6,10.6l0,121.4c0,5.9-4.7,10.6-10.6,10.6H202.2
-		  c-5.9,0-10.6-4.7-10.6-10.6v-13.9c0-5.9,4.9-10.7,10.8-10.6l73.5,1.8l-89.8-89.8c-4.2-4.2-4.2-10.8,0-15l10-10
-		  c4.2-4.2,10.8-4.2,15,0l89.8,89.8l-1.8-73.5C298.9,190.6,303.7,185.7,309.7,185.7L309.7,185.7z"/>
-	  </g>
-  </svg>
+		<g id="Layer_2">
+			<path class="st1" d="M309.7,185.7h13.9c5.9,0,10.6,4.7,10.6,10.6l0,121.4c0,5.9-4.7,10.6-10.6,10.6H202.2
+			c-5.9,0-10.6-4.7-10.6-10.6v-13.9c0-5.9,4.9-10.7,10.8-10.6l73.5,1.8l-89.8-89.8c-4.2-4.2-4.2-10.8,0-15l10-10
+			c4.2-4.2,10.8-4.2,15,0l89.8,89.8l-1.8-73.5C298.9,190.6,303.7,185.7,309.7,185.7L309.7,185.7z"/>
+		</g>
+	</svg>
+	</div> <!-- ==================== --> <!-- end cta -->
+	
+	</div> <!--end content -->
+	<!-- </div>end callout -->
+	</a>
 
-   </div> <!-- ==================== --> <!-- end cta -->
 
-</div> <!--end content -->
-</div><!--end callout-->
-
-<div class="callout dine">
+	<a class="callout incentives-resources" href="/business-support/incentives/">
+<!-- <div class="callout incentives-resources"> -->
 <div class="bgimage"></div>
 <div class="content">
   <div class="lockup">
@@ -92,9 +93,10 @@ get_header();
    </div> <!-- ==================== --> <!-- end cta -->
 
 </div> <!--end content -->
-</div><!--end callout-->
+</a><!--end callout-->
 
-<div class="callout art">
+<a class="callout covid" href="/business-support/covid-19-resources/">
+<!-- <div class="callout covid"> -->
 <div class="bgimage"></div>
 <div class="content">
   <div class="lockup">
@@ -125,9 +127,10 @@ get_header();
    </div> <!-- ==================== --> <!-- end cta -->
 
 </div> <!--end content -->
-</div><!--end callout-->
+</a><!--end callout-->
 
-<div class="callout live">
+<a class="callout support-artists" href="/support-for-artists/">
+<!-- <div class="callout support-artists"> -->
 <div class="bgimage"></div>
 <div class="content">
   <div class="lockup">
@@ -157,22 +160,19 @@ get_header();
    </div> <!-- ==================== --> <!-- end cta -->
 
 </div> <!--end content -->
-</div><!--end callout-->
+</a><!--end callout-->
 
-</div>
+</div> <!-- end callouts -->
 
-</div> <!-- end page-business-support -->
+</div> <!-- end page-business-support and custom-content -->
 
 
 
 	
 
-
+<div class="loop-no-header">
 <?php
-
-
 /* THE LOOP */
-
 while ( have_posts() ) :
 	the_post();
 	get_template_part( 'template-parts/content/content-page' );
@@ -184,4 +184,7 @@ while ( have_posts() ) :
 endwhile; 
 // End of the loop.
 
+?>
+</div> <!-- end loop-no-header-->
+<?php
 get_footer();
