c193b1854f472fa19c0987c7f449723f56e9e482
Grant Corley <grantcorley@gmail.com>
2021-06-01 08:43:16 -0400

    set up basic templates for biz directory and en espanol pages

---
+++ page-business-directory.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * Template Name: Page: Business Directory
+ * 
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+
+<div class="page-business-directory custom-content">
+
+<article class="page hardcoded type-page status-publish hentry entry">
+
+			<header class="entry-header hardcoded alignwide">
+			<h1 class="entry-title hardcoded">Business Directory</h1>			
+			<!-- <figure class="post-thumbnail"> -->
+			</header><!-- .entry-header -->
+	
+</article>
+	
+
+
+	<div class="callouts">
+
+	<a class="callout available-buildings" href="/business-directory/art/">
+	<div class="bgimage"></div>
+	<div class="content">
+	<div class="lockup">
+		<h2>Art</h2>
+	</div><!--end lockup-->
+
+	<div class="cta"> <!-- ==================== -->
+	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+			viewBox="0 0 356 355" style="enable-background:new 0 0 356 355;" xml:space="preserve">
+		<style type="text/css">
+			.st0{fill:#F38F1D;}
+			.st1{fill:#FFFFFF;}
+		</style>
+		<g id="Layer_1" focusable="false">
+			<g>
+			<polygon class="st0" points="355.9,0 0.9,355 355.9,355 		"/>
+			</g>
+		</g>
+		<g id="Layer_2">
+			<path class="st1" d="M309.7,185.7h13.9c5.9,0,10.6,4.7,10.6,10.6l0,121.4c0,5.9-4.7,10.6-10.6,10.6H202.2
+			c-5.9,0-10.6-4.7-10.6-10.6v-13.9c0-5.9,4.9-10.7,10.8-10.6l73.5,1.8l-89.8-89.8c-4.2-4.2-4.2-10.8,0-15l10-10
+			c4.2-4.2,10.8-4.2,15,0l89.8,89.8l-1.8-73.5C298.9,190.6,303.7,185.7,309.7,185.7L309.7,185.7z"/>
+		</g>
+	</svg>
+	</div> <!-- ==================== --> <!-- end cta -->
+	
+	</div> <!--end content -->
+	<!-- </div>end callout -->
+	</a>
+
+
+	<a class="callout incentives-resources" href="/business-directory/nightlife-dining/">
+<!-- <div class="callout incentives-resources"> -->
+<div class="bgimage"></div>
+<div class="content">
+  <div class="lockup">
+	<h2>Nightlife &amp; Dining</h2>
+  </div><!--end lockup-->
+
+  <div class="cta"> <!-- ==================== -->
+	
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+		viewBox="0 0 356 355" style="enable-background:new 0 0 356 355;" xml:space="preserve">
+	  <style type="text/css">
+		.st0{fill:#F38F1D;}
+		.st1{fill:#FFFFFF;}
+	  </style>
+	  <g id="Layer_1" focusable="false">
+		<g>
+		  <polygon class="st0" points="355.9,0 0.9,355 355.9,355 		"/>
+		</g>
+	  </g>
+	  <g id="Layer_2">
+		<path class="st1" d="M309.7,185.7h13.9c5.9,0,10.6,4.7,10.6,10.6l0,121.4c0,5.9-4.7,10.6-10.6,10.6H202.2
+		  c-5.9,0-10.6-4.7-10.6-10.6v-13.9c0-5.9,4.9-10.7,10.8-10.6l73.5,1.8l-89.8-89.8c-4.2-4.2-4.2-10.8,0-15l10-10
+		  c4.2-4.2,10.8-4.2,15,0l89.8,89.8l-1.8-73.5C298.9,190.6,303.7,185.7,309.7,185.7L309.7,185.7z"/>
+	  </g>
+  </svg>
+
+   </div> <!-- ==================== --> <!-- end cta -->
+
+</div> <!--end content -->
+</a><!--end callout-->
+
+<a class="callout covid" href="/business-directory/shopping/">
+<!-- <div class="callout covid"> -->
+<div class="bgimage"></div>
+<div class="content">
+  <div class="lockup">
+  <h2>Shopping</h2>
+	
+  </div><!--end lockup-->
+
+  <div class="cta"> <!-- ==================== -->
+	
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+		viewBox="0 0 356 355" style="enable-background:new 0 0 356 355;" xml:space="preserve">
+	  <style type="text/css">
+		.st0{fill:#F38F1D;}
+		.st1{fill:#FFFFFF;}
+	  </style>
+	  <g id="Layer_1" focusable="false">
+		<g>
+		  <polygon class="st0" points="355.9,0 0.9,355 355.9,355 		"/>
+		</g>
+	  </g>
+	  <g id="Layer_2">
+		<path class="st1" d="M309.7,185.7h13.9c5.9,0,10.6,4.7,10.6,10.6l0,121.4c0,5.9-4.7,10.6-10.6,10.6H202.2
+		  c-5.9,0-10.6-4.7-10.6-10.6v-13.9c0-5.9,4.9-10.7,10.8-10.6l73.5,1.8l-89.8-89.8c-4.2-4.2-4.2-10.8,0-15l10-10
+		  c4.2-4.2,10.8-4.2,15,0l89.8,89.8l-1.8-73.5C298.9,190.6,303.7,185.7,309.7,185.7L309.7,185.7z"/>
+	  </g>
+  </svg>
+
+   </div> <!-- ==================== --> <!-- end cta -->
+
+</div> <!--end content -->
+</a><!--end callout-->
+
+
+</div> <!-- end callouts -->
+
+</div> <!-- end page-business-support and custom-content -->
+
+
+
+	
+
+<div class="loop-no-header">
+<?php
+/* THE LOOP */
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content/content-page' );
+
+	 //If comments are open or there is at least one comment, load up the comment template.
+	//if ( comments_open() || get_comments_number() ) {
+		//comments_template();
+	//}
+endwhile; 
+// End of the loop.
+
+?>
+</div> <!-- end loop-no-header-->
+<?php
+get_footer();
---
+++ page-en-espanol.php
@@ -0,0 +1,226 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * Template Name: Page: En Espanol
+ * 
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+?>
+
+<div class="page-en-espanol custom-content">
+
+<article class="page hardcoded type-page status-publish hentry entry">
+
+			<header class="entry-header hardcoded alignwide">
+			<h1 class="entry-title hardcoded">En Español</h1>			
+			<!-- <figure class="post-thumbnail"> -->
+			</header><!-- .entry-header -->
+	
+</article>
+	
+
+
+	<div class="callouts">
+
+<!-- 01 -->
+	<a class="callout acerca-de" href="/en-espanol/acerca-de/">
+	<div class="bgimage"></div>
+	<div class="content">
+	<div class="lockup">
+		<h2>Acerca de</h2>
+	</div><!--end lockup-->
+
+	<div class="cta"> <!-- ==================== -->
+	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+			viewBox="0 0 356 355" style="enable-background:new 0 0 356 355;" xml:space="preserve">
+		<style type="text/css">
+			.st0{fill:#F38F1D;}
+			.st1{fill:#FFFFFF;}
+		</style>
+		<g id="Layer_1" focusable="false">
+			<g>
+			<polygon class="st0" points="355.9,0 0.9,355 355.9,355 		"/>
+			</g>
+		</g>
+		<g id="Layer_2">
+			<path class="st1" d="M309.7,185.7h13.9c5.9,0,10.6,4.7,10.6,10.6l0,121.4c0,5.9-4.7,10.6-10.6,10.6H202.2
+			c-5.9,0-10.6-4.7-10.6-10.6v-13.9c0-5.9,4.9-10.7,10.8-10.6l73.5,1.8l-89.8-89.8c-4.2-4.2-4.2-10.8,0-15l10-10
+			c4.2-4.2,10.8-4.2,15,0l89.8,89.8l-1.8-73.5C298.9,190.6,303.7,185.7,309.7,185.7L309.7,185.7z"/>
+		</g>
+	</svg>
+	</div> <!-- ==================== --> <!-- end cta -->
+	
+	</div> <!--end content -->
+	</a>
+
+<!-- 02 -->
+	<a class="callout incentives-resources" href="/en-espanol/abrir-un-negocio/">
+<div class="bgimage"></div>
+<div class="content">
+  <div class="lockup">
+	<h2>Abrir Un Negocio</h2>
+  </div><!--end lockup-->
+
+  <div class="cta"> <!-- ==================== -->
+	
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+		viewBox="0 0 356 355" style="enable-background:new 0 0 356 355;" xml:space="preserve">
+	  <style type="text/css">
+		.st0{fill:#F38F1D;}
+		.st1{fill:#FFFFFF;}
+	  </style>
+	  <g id="Layer_1" focusable="false">
+		<g>
+		  <polygon class="st0" points="355.9,0 0.9,355 355.9,355 		"/>
+		</g>
+	  </g>
+	  <g id="Layer_2">
+		<path class="st1" d="M309.7,185.7h13.9c5.9,0,10.6,4.7,10.6,10.6l0,121.4c0,5.9-4.7,10.6-10.6,10.6H202.2
+		  c-5.9,0-10.6-4.7-10.6-10.6v-13.9c0-5.9,4.9-10.7,10.8-10.6l73.5,1.8l-89.8-89.8c-4.2-4.2-4.2-10.8,0-15l10-10
+		  c4.2-4.2,10.8-4.2,15,0l89.8,89.8l-1.8-73.5C298.9,190.6,303.7,185.7,309.7,185.7L309.7,185.7z"/>
+	  </g>
+  </svg>
+
+   </div> <!-- ==================== --> <!-- end cta -->
+
+</div> <!--end content -->
+</a><!--end callout-->
+
+<!-- 03 -->
+<a class="callout covid" href="/en-espanol/explorar/">
+<div class="bgimage"></div>
+<div class="content">
+  <div class="lockup">
+  <h2>Explorar</h2>
+	
+  </div><!--end lockup-->
+
+  <div class="cta"> <!-- ==================== -->
+	
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+		viewBox="0 0 356 355" style="enable-background:new 0 0 356 355;" xml:space="preserve">
+	  <style type="text/css">
+		.st0{fill:#F38F1D;}
+		.st1{fill:#FFFFFF;}
+	  </style>
+	  <g id="Layer_1" focusable="false">
+		<g>
+		  <polygon class="st0" points="355.9,0 0.9,355 355.9,355 		"/>
+		</g>
+	  </g>
+	  <g id="Layer_2">
+		<path class="st1" d="M309.7,185.7h13.9c5.9,0,10.6,4.7,10.6,10.6l0,121.4c0,5.9-4.7,10.6-10.6,10.6H202.2
+		  c-5.9,0-10.6-4.7-10.6-10.6v-13.9c0-5.9,4.9-10.7,10.8-10.6l73.5,1.8l-89.8-89.8c-4.2-4.2-4.2-10.8,0-15l10-10
+		  c4.2-4.2,10.8-4.2,15,0l89.8,89.8l-1.8-73.5C298.9,190.6,303.7,185.7,309.7,185.7L309.7,185.7z"/>
+	  </g>
+  </svg>
+
+   </div> <!-- ==================== --> <!-- end cta -->
+
+</div> <!--end content -->
+</a><!--end callout-->
+
+
+<!-- 04 -->
+<a class="callout support-artists" href="/en-espanol/eventos/">
+<div class="bgimage"></div>
+<div class="content">
+  <div class="lockup">
+	<h2>Eventos</h2>
+  </div><!--end lockup-->
+
+  <div class="cta"> <!-- ==================== -->
+	
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+		viewBox="0 0 356 355" style="enable-background:new 0 0 356 355;" xml:space="preserve">
+	  <style type="text/css">
+		.st0{fill:#F38F1D;}
+		.st1{fill:#FFFFFF;}
+	  </style>
+	  <g id="Layer_1" focusable="false">
+		<g>
+		  <polygon class="st0" points="355.9,0 0.9,355 355.9,355 		"/>
+		</g>
+	  </g>
+	  <g id="Layer_2">
+		<path class="st1" d="M309.7,185.7h13.9c5.9,0,10.6,4.7,10.6,10.6l0,121.4c0,5.9-4.7,10.6-10.6,10.6H202.2
+		  c-5.9,0-10.6-4.7-10.6-10.6v-13.9c0-5.9,4.9-10.7,10.8-10.6l73.5,1.8l-89.8-89.8c-4.2-4.2-4.2-10.8,0-15l10-10
+		  c4.2-4.2,10.8-4.2,15,0l89.8,89.8l-1.8-73.5C298.9,190.6,303.7,185.7,309.7,185.7L309.7,185.7z"/>
+	  </g>
+  </svg>
+
+   </div> <!-- ==================== --> <!-- end cta -->
+
+</div> <!--end content -->
+</a><!--end callout-->
+
+
+
+<!-- 05 -->
+<a class="callout support-artists" href="/en-espanol/contacto/">
+
+<div class="bgimage"></div>
+<div class="content">
+  <div class="lockup">
+	<h2>Contacto</h2>
+  </div><!--end lockup-->
+
+  <div class="cta"> <!-- ==================== -->
+	
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+		viewBox="0 0 356 355" style="enable-background:new 0 0 356 355;" xml:space="preserve">
+	  <style type="text/css">
+		.st0{fill:#F38F1D;}
+		.st1{fill:#FFFFFF;}
+	  </style>
+	  <g id="Layer_1" focusable="false">
+		<g>
+		  <polygon class="st0" points="355.9,0 0.9,355 355.9,355 		"/>
+		</g>
+	  </g>
+	  <g id="Layer_2">
+		<path class="st1" d="M309.7,185.7h13.9c5.9,0,10.6,4.7,10.6,10.6l0,121.4c0,5.9-4.7,10.6-10.6,10.6H202.2
+		  c-5.9,0-10.6-4.7-10.6-10.6v-13.9c0-5.9,4.9-10.7,10.8-10.6l73.5,1.8l-89.8-89.8c-4.2-4.2-4.2-10.8,0-15l10-10
+		  c4.2-4.2,10.8-4.2,15,0l89.8,89.8l-1.8-73.5C298.9,190.6,303.7,185.7,309.7,185.7L309.7,185.7z"/>
+	  </g>
+  </svg>
+
+   </div> <!-- ==================== --> <!-- end cta -->
+
+</div> <!--end content -->
+</a><!--end callout-->
+
+</div> <!-- end callouts -->
+
+</div> <!-- end page-business-support and custom-content -->
+
+
+
+	
+
+<div class="loop-no-header">
+<?php
+/* THE LOOP */
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content/content-page' );
+
+	 //If comments are open or there is at least one comment, load up the comment template.
+	//if ( comments_open() || get_comments_number() ) {
+		//comments_template();
+	//}
+endwhile; 
+// End of the loop.
+
+?>
+</div> <!-- end loop-no-header-->
+<?php
+get_footer();
